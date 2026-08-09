0e96401b8955833e6a2dd202143acfa5bc5f6356
Aryan Shah <aryan.n.shah2505@gmail.com>
2026-08-09 16:49:34 -0400

    Tournament bracket update

--- frontend/src/Tournament.jsx
+++ frontend/src/Tournament.jsx
@@ -92,11 +92,11 @@ const Tournament = ({ players = [], showSeedBoard, setShowSeedBoard, showAbout, 
 
   const FUTURE_ROUNDS = {
     round2: { 
-      match1: { p1: { ...getPlayerBySeed(1), score: null }, p2: { ...getPlayerBySeed(3), score: null } }, 
+      match1: { p1: { ...getPlayerBySeed(1), score: 2 }, p2: { ...getPlayerBySeed(3), score: 0 } }, 
       match2: { p1: { ...getPlayerBySeed(2), score: null }, p2: { ...getPlayerBySeed(4), score: null } } 
     },
     round3: { 
-      match1: { p1: { name: "TBD", seed: "-", score: null }, p2: { name: "TBD", seed: "-", score: null } } 
+      match1: { p1: { ...getPlayerBySeed(1), score: null }, p2: { name: "TBD", seed: "-", score: null } } 
     }
   };
 
